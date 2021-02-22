3df5e110c22a15dd1a2185966737d2c0fb016b17
西口広海 <hiromi.n.cl.3s6@gmail.com>
2021-02-22 17:27:45 +0900

    serverRoot修正

--- src/service/FileScraper.js
+++ src/service/FileScraper.js
@@ -1,11 +1,12 @@
 export class FileScraper {
   constructor({ url }) {
-    this.serverRoot = "localhost:5500"
+    // this.serverRoot = "http://localhost:5500"
+    this.serverRoot = "https://boiling-dusk-96001.herokuapp.com"
     this.url = url
   }
 
   async fetch() {
-    this.res = await fetch(`http://${this.serverRoot}/pdf?url=${this.url}`)
+    this.res = await fetch(`${this.serverRoot}/pdf?url=${this.url}`)
     if (!this.res.ok) {
       throw this.res
     }
